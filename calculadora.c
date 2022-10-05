241470cf462a8cda71f17f9dd935cc93fac9cb51
caiomwn <115113632+caiomwn@users.noreply.github.com>
2022-10-05 19:09:14 -0300

    Create calculadora.c
    Projeto de atividade de produção da cadeira de Algoritmos e Programação, La Salle.

---
+++ calculadora.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+void main()
+{
+    setlocale(LC_ALL,"Portuguese_Brazil");
+    /*
+    GitHub: caiomwn
+    O código a seguir cria uma calculadora, onde o usuário escolhe uma operação e depois insere os valores. O código verifica se o usuário
+    tentou dividir por zero pra evitar erros de execução.
+    */
+
+    int operacao;1
+    float n1, n2, resultado;
+
+    printf("Iniciando a calculadora.\n");
+
+            do
+        {
+            printf("\nSelecione a operacao desejada:\n\n");
+            printf("\n1 - Adicao\n 2 - Subtracao\n 3 - Divisao\n 4 - Multiplicacao\n 5 - Raiz quadrada\n 6 - Potenciacao\n\n");
+
+            scanf("%d", &operacao);
+
+            if(operacao > 6 || operacao < 1)
+            {
+                printf("\nValor invalido! Digite outro valor.\n\n");
+            }
+        } while(operacao > 6 || operacao < 1);
+
+        if (operacao == 5)
+        {
+            printf("\nDigite o valor da raiz quadrada: ");
+            scanf("%f", &n1);
+        }
+        else
+        {
+            do
+            {
+                printf("\nEscolha o primeiro valor para a operacao: ");
+                scanf("%f", &n1);
+                printf("\nEscolha o segundo valor para a operacao: ");
+                scanf("%f", &n2);
+
+                if (operacao == 3 && n2 == 0)
+                {
+                    printf("Erro! E impossivel dividir por 0, tente novamente.\n");
+                }
+            } while(operacao == 3 && n2 == 0);
+        }
+
+        switch(operacao)
+        {
+            case 1:
+                resultado = n1+n2;
+                break;
+            case 2:
+                resultado = n1-n2;
+                break;
+            case 3:
+                resultado = n1/n2;
+                break;
+            case 4:
+                resultado = n1*n2;
+                break;
+            case 5:
+                resultado = sqrt(n1);
+                break;
+            case 6:
+                resultado = pow(n1, n2);
+                break;
+        }
+
+        printf("\nO resultado da operacao e: %.01f \n\n", resultado);
+        printf("\nE se o verdadeiro calculo foram os amigos que fizemos no caminho?\n\n");
+        }
